b67fa12f3fdc42083348f2e01e2fb2de5e9929bf
johannes@debussy <johannes@debussy>
2012-11-24 19:29:48 +0100

    a

--- best-design-tuts/download.sh
+++ best-design-tuts/download.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -O http://www.bestdesigntuts.com/wp-content/uploads/2012/09/HD-wallpaper-[1-10].jpg 
+curl -O http://www.bestdesigntuts.com/wp-content/uploads/2012/09/HD-Wallpaper-1.jpg       \
+      -O  http://www.bestdesigntuts.com/wp-content/uploads/2012/09/HD-wallpaper-2.jpeg    \
+      -O http://www.bestdesigntuts.com/wp-content/uploads/2012/09/HD-wallpaper-[3-10].jpg  
